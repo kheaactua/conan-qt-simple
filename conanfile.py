874263ec0752c45670ba1e72bfe8086d697bbb97
Matthew Russell <mrussell@neptec.com>
2018-10-11 13:47:30 -0400

    Removed package stuff that is not relevant (or helpful) with a user-installed Qt

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 # -*- coding: future_fstrings -*-
 # -*- coding: utf-8 -*-
 
-import re, os, glob
+import os
 from conans import ConanFile, tools
 from conans.errors import ConanException
 from conans.model.version import Version
@@ -82,45 +82,18 @@ class QtConan(ConanFile):
         self.output.info(f'Found Qt at {qt_path}')
 
         self.cpp_info.libdirs     = [os.path.join(qt_path, 'lib')]
-        self.cpp_info.libs        = tools.collect_libs(self, folder=self.cpp_info.libdirs[0])
+        self.cpp_info.libs        = tools.collect_libs(self, folder=self.cpp_info.libdirs[0]) # v1.8.0 makes this arg unnecessary
 
         self.cpp_info.bindirs     = [os.path.join(qt_path, 'bin')]
         self.cpp_info.includedirs = [os.path.join(qt_path, 'include')]
 
-        self.cpp_info.resdirs      = [os.path.join(qt_path, 'lib', 'cmake')]
+        self.cpp_info.resdirs     = [os.path.join(qt_path, 'lib', 'cmake')]
 
         # Put qmake and DLLs in the path
-        if self.settings.os == "Windows":
+        if tools.os_info.is_windows:
             self.env_info.path.extend(self.cpp_info.bindirs)
 
         # Make it easier for CMake to find Qt
         self.env_info.CMAKE_PREFIX_PATH.append(self.cpp_info.resdirs[0])
-
-        if 'Linux' == self.settings.os:
-
-            # Attempt to fix the uic LD_LIBRARY_PATH issues that I can't seem
-            # to address through CMake
-            self.env_info.LD_LIBRARY_PATH.append(self.cpp_info.libdirs[0])
-
-            # Qt appears to hard code the font path which leads to run time
-            # errors
-            self.env_info.QT_QPA_FONTDIR = os.path.join(self.cpp_info.libdirs[0], 'fonts')
-
-            # Populate the pkg-config environment variables.  This is Linux
-            # only because on Windows there's a different system (.prl files..
-            # *shrugs*)
-            with tools.pythonpath(self):
-                from platform_helpers import adjustPath, appendPkgConfigPath
-
-                pkg_config_path = os.path.join(self.package_folder, 'lib', 'pkgconfig')
-                appendPkgConfigPath(adjustPath(pkg_config_path), self.env_info)
-
-                pc_files = glob.glob(adjustPath(os.path.join(pkg_config_path, '*.pc')))
-                for f in pc_files:
-                    p_name = re.sub(r'\.pc$', '', os.path.basename(f))
-                    p_name = re.sub(r'\W', '_', p_name.upper())
-                    setattr(self.env_info, f'PKG_CONFIG_{p_name}_PREFIX', adjustPath(self.package_folder))
-
-                appendPkgConfigPath(adjustPath(pkg_config_path), self.env_info)
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
